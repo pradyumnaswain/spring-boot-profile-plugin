2ad70f340c3001b9a7717301274a8bb455b82f06
GitHub Action <action@github.com>
2023-03-22 17:37:17 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "spring-boot-profile-plugin"
--- src/main/kotlin/com/github/pradyumnaswain/springbootprofileplugin/MyBundle.kt
+++ src/main/kotlin/com/github/pradyumnaswain/springbootprofileplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.pradyumnaswain.springbootprofileplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
